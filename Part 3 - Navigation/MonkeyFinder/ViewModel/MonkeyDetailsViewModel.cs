5afc2defceacae8d8e6e908a933deb900a99a844
Sandeep Sagar Panjala <shravanreddy199@gmail.com>
2023-12-08 02:22:03 -0600

    Detail page, services and detailed view model pages are finished

---
+++ Part 3 - Navigation/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MonkeyFinder.ViewModel;
+
+[QueryProperty(nameof(Monkey), "Monkey")]
+public partial class MonkeyDetailsViewModel : BaseViewModel
+{
+    [ObservableProperty]
+    Monkey monkey;
+
+    public MonkeyDetailsViewModel() {}
+}
